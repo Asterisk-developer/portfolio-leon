4c486dd9088e1caed6c423496081ed3dc756b795
Asterisk-developer <asteriskdeveloper.dev@gmail.com>
2025-07-15 23:46:35 +0530

    'portfolio-basic-layout'

---
+++ src/shared/material.ts
@@ -0,0 +1,15 @@
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatIconModule } from "@angular/material/icon";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatTooltipModule } from "@angular/material/tooltip";
+
+export const materialImports = [
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatChipsModule,
+  MatTooltipModule,
+]
